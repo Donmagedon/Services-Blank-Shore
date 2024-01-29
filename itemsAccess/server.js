780d8f8f0da88aa8318bfa58d5ecdeacbe5f4ef8
donmagedon <donmagedon7@gmail.com>
2024-01-29 18:04:11 -0500

    adding env config

--- itemsAccess/server.js
+++ itemsAccess/server.js
@@ -3,12 +3,13 @@ const app = express()
 const items = require("./routes/items")
 const port = 2020
 const test = require("./mongoDB")
-
+const dotenv = require("dotenv")
+dotenv.config()
 app.get("/",(req,res)=>{
   res.send("it be working")
   })
 
 app.use("/items",items)
 app.listen(port,"0.0.0.0",()=>{
-  console.log("done!")
+  console.log(process.env.MONGODB_PASSWORD)
 })
