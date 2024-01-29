a9089e23ed7b23d32020c8d4f04b5e6dd3dd270a
donmagedon <donmagedon7@gmail.com>
2024-01-29 15:35:51 -0500

    VM access

--- itemsAccess/server.js
+++ itemsAccess/server.js
@@ -4,7 +4,7 @@ const items = require("./routes/items")
 const port = 3330
 const test = require("./mongoDB")
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', 'http://localhost:1510');
+  res.header('Access-Control-Allow-Origin', process.env.VM_IP);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next()
