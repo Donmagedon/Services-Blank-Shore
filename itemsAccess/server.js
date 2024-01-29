9dcc28e14492dd6cd5f3cc10afe6d30e4fa859cf
donmagedon <donmagedon7@gmail.com>
2024-01-29 15:38:50 -0500

    VM access removed

--- itemsAccess/server.js
+++ itemsAccess/server.js
@@ -4,9 +4,7 @@ const items = require("./routes/items")
 const port = 3330
 const test = require("./mongoDB")
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', process.env.VM_IP);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
   next()
 })
 app.get("/",(req,res)=>{
