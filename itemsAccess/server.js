3f8c3508bb3b9aa9455470a29d0ce5e12cd580d0
donmagedon <donmagedon7@gmail.com>
2024-01-29 23:44:49 -0500

    adding allow origin

--- itemsAccess/server.js
+++ itemsAccess/server.js
@@ -6,7 +6,11 @@ const test = require("./mongoDB")
 const dotenv = require("dotenv")
 dotenv.config()
 
-
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin", "http://34.172.199.162")
+  next()
+}
+)
  
 app.get("/",(req,res)=>{
   res.send("it be working")
