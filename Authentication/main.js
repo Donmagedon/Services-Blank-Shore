92b235686b3ceb3765326223d3f9d86134652cc8
donmagedon <donmagedon7@gmail.com>
2024-01-30 11:32:25 -0500

    added final host

--- Authentication/main.js
+++ Authentication/main.js
@@ -8,7 +8,7 @@ const tokenAuthenticate = require("./routes/tokenAuthentication")
 
 app.use((req,res,next)=>{
     
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:1510")
+    res.setHeader("Access-Control-Allow-Origin","https://blank-shore.web.app")
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
--- itemsAccess/server.js
+++ itemsAccess/server.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:1510")
+  res.setHeader("Access-Control-Allow-Origin", "https://blank-shore.web.app")
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
